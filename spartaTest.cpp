c5d0f9728bbc38f1b78f32931ce67b4a44240ece
mfacchinelli <michele.facchinelli@yahoo.it>
2018-04-27 21:13:57 +0200

    Added test for SPARTA interface with Tudat

---
+++ spartaTest.cpp
@@ -0,0 +1,61 @@
+/*    Copyright (c) 2010-2017, Delft University of Technology
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ */
+
+#include <ctime>
+#include <Tudat/SimulationSetup/tudatSimulationHeader.h>
+
+//! Get path for output directory.
+static inline std::string getOutputPath(
+        const std::string& extraDirectory = "" )
+{
+    // Declare file path string assigned to filePath.
+    // __FILE__ only gives the absolute path of the header file!
+    std::string filePath_( __FILE__ );
+
+    // Strip filename from temporary string and return root-path string.
+    std::string reducedPath = filePath_.substr( 0, filePath_.length( ) -
+                                                std::string( "spartaTest.cpp" ).length( ) );
+    std::string outputPath = reducedPath + "SimulationOutput/";
+    if ( extraDirectory != "" )
+    {
+        outputPath += extraDirectory;
+    }
+
+    if ( outputPath.at( outputPath.size( ) - 1 ) != '/' )
+    {
+        outputPath += "/";
+    }
+
+    return outputPath;
+}
+
+//! Execute propagation of orbit of Satellite around the Earth.
+int main( )
+{
+    using namespace tudat;
+    using namespace tudat::simulation_setup;
+    using namespace tudat::propagators;
+    using namespace tudat::numerical_integrators;
+    using namespace tudat::orbital_element_conversions;
+    using namespace tudat::basic_mathematics;
+    using namespace tudat::gravitation;
+    using namespace tudat::numerical_integrators;
+    using namespace tudat::estimatable_parameters;
+    using namespace tudat::ephemerides;
+    using namespace tudat::input_output;
+    using namespace tudat::aerodynamics;
+    using namespace tudat::root_finders;
+
+
+
+    // Final statement.
+    // The exit code EXIT_SUCCESS indicates that the program was successfully executed.
+    return EXIT_SUCCESS;
+}
